d4cc666ff035de728029225a78f0084daa25ae15
Guilherme <guilhermerochas0420@gmail.com>
2026-03-08 23:16:08 -0300

    feat: added 'users' table

--- src/database/migrations.js
+++ src/database/migrations.js
@@ -22,6 +22,14 @@ async function runMigrations() {
       );
     `);
 
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id           SERIAL         PRIMARY KEY,
+        username     VARCHAR(255)   NOT NULL UNIQUE,
+        password     VARCHAR(255)   NOT NULL
+      );
+    `);
+
     console.log('Migrations ran successfully.');
   } finally {
     client.release();
